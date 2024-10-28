1eee98b62eef752fab4d542964af27b8093c72f8
thisAbdu <abdulfetaheishak@gmail.com>
2024-10-28 21:50:25 +0300

    add controller test

--- tests/stream_controller_test.go
+++ tests/stream_controller_test.go
@@ -1,8 +1,12 @@
 package tests
 
 import (
+	"bytes"
+	"encoding/json"
 	"go-redpanda-streaming/controller"
+	"go-redpanda-streaming/domain"
 	"go-redpanda-streaming/domain/mocks"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -36,7 +40,63 @@ func TestStartStreamController(t *testing.T){
 		assert.Equal(t, http.StatusOK, w.Code)
 	})
 }
+func TestSendDataController(t *testing.T) {
+    t.Run("success", func(t *testing.T) {
+        mockUsecase := new(mocks.StreamUsecase)
+        mockAPIKeyStore := new(mocks.APIKeyStore)
+        mockStreamID := "testID"
+        mockData := domain.StreamData{Data: "testData"}
 
-func TestSendDataController(t *testing.T){
-	t.Run("")
+        mockUsecase.On("SendData", mockStreamID, mockData).Return(nil)
+
+        controller := controller.NewStreamController(mockUsecase, mockAPIKeyStore)
+        router := gin.Default()
+
+        router.POST("/stream/send/:stream_id", controller.SendData)
+
+        jsonData, err := json.Marshal(mockData)
+        if err != nil {
+            t.Fatal(err)
+        }
+
+        req, err := http.NewRequest("POST", "/stream/send/"+mockStreamID, bytes.NewBuffer(jsonData))
+        if err != nil {
+			log.Println("this si the error", err)
+            t.Fatal(err)
+        }
+        req.Header.Set("Content-Type", "application/json")
+
+        w := httptest.NewRecorder()
+        router.ServeHTTP(w, req)
+
+        assert.Equal(t, http.StatusOK, w.Code)
+    })
+}
+
+func TestGetResultsController(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		mockUsecase := new(mocks.StreamUsecase)
+		mockAPIKeyStore := new(mocks.APIKeyStore)
+		mockStreamID := "testID"
+		mockMessages := []domain.Message{
+			{StreamID: "testID", Payload: "testPayload"},
+		}
+
+		mockUsecase.On("GetResults", mockStreamID).Return(mockMessages, nil)
+
+		controller := controller.NewStreamController(mockUsecase, mockAPIKeyStore)
+		router := gin.Default()
+
+		router.GET("/stream/results/:stream_id", controller.GetResults)
+
+		req, err := http.NewRequest("GET", "/stream/results/"+mockStreamID, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		w := httptest.NewRecorder()
+		router.ServeHTTP(w, req)
+
+		assert.Equal(t, http.StatusOK, w.Code)
+	})
 }
